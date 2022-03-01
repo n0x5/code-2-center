f8dc27e7891a9b05d6ad0ee27269be1da85aa209
n0x5 <n0x5@users.noreply.github.com>
2022-03-01 11:26:31 +0100

    new breadcrumbs code

--- page.php
+++ page.php
@@ -1,50 +1,36 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-
-	
+    
 <div id="main">
-
 <div id="header">
 <a href="/blog"><img src="<?php echo get_template_directory_uri(); ?>/images/mclogo2.jpg" /></a>
 <?php get_header(); ?>
 </div>
 <div id="sidebar"><?php get_sidebar(); ?></div>
 <div id="content">
+
+<?php 
+$parents = get_post_ancestors( $post->ID );
+foreach ($parents as $value) {
+    $title = get_the_title($value);
+    $page_link = get_page_link($value);
+    $page_url = '<a href=' . $page_link .'>' . $title . '</a>';
+    $item_output1 = $page_url .' -> '. $item_output1;
+    } ?>
+   
 <?php
-if ( $post->post_parent ) {
-    $children = wp_list_pages( array(
-        'title_li' => '',
-        'child_of' => $post->post_parent,
-        'echo'     => 0
-    ) );
-   $page_link = get_page_link( $post->post_parent );
-    $title = get_the_title( $post->post_parent );
-} else {
-    $children = wp_list_pages( array(
-        'title_li' => '',
-        'child_of' => $post->ID,
-        'echo'     => 0
-    ) );
-    $page_link = get_page_link( $post->post_parent );
-    $title = get_the_title( $post->ID );
-}
+$title2 = get_the_title($post->ID);
+$page_link2 = get_page_link($post->ID);
+$page_url2 = '<a href=' . $page_link2 .'>' . $title2 . '</a>';
+$url = get_permalink(get_option('page_for_posts' ));
+$title3 = get_the_title($url);
+$page_url3 = '<a href=' . $url .'>' . 'Home' . '</a>';
+echo '<h2>' . $page_url3 .' -> '. $item_output1 . $page_url2 . '</h2>';
+?>
 
-if ( $post->post_parent ) : ?>
-    <h2><?php // echo $title ; ?></h2>
-    <h2><?php echo '<a href="/blog">Home</a> -> <a href=' . $page_link .'>' . $title . '</a>'; ?> -> <?php the_title(); ?></h2>
-    <ul>
-        <?php // echo $children; ?>
-    </ul>
-
-<?php else : ?>
-<h2><a href="/blog">Home</a></h2>
-
-<h2 style="text-align: center;"> -> <?php the_title() ; ?></h2>
-
-<?php endif; ?>
 <?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?>
-	
+    
 <div class="post">
 <?php the_content('-> read more'); ?>
 
